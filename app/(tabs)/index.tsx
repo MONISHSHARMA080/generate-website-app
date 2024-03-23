cbb9e05f8d7538c13a76e15c2095c8874c3b67bb
Monish <monishsharma080@gmail.com>
2024-03-23 23:37:03 +0530

    fixed curved-from-top-view moving up on text input fixed

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,12 @@
 
 import * as React from 'react';
 import HomeScreen from '@/components/Home/HomeScreen';
+import SignInScreen from '@/components/auth/SignInScreen';
 
 export default function TabOneScreen() {
   return (
     <HomeScreen />
+    // <SignInScreen />
   );
 }
 
--- components/Home/HomeScreen.tsx
+++ components/Home/HomeScreen.tsx
@@ -3,13 +3,52 @@ import * as React from 'react'
 
 export default function HomeScreen() {
   return (
-    <View className='flex-1 bg-gray-800 rounded-2xl relative'>
-       <View className='absolute bottom-0 left-0 right-0 px-4 pb-4 flex items-center justify-center'>
-        <TextInput className='z-10   border-2 px-4 w-96  rounded-3xl  py-4 border-gray-100 h-auto max-h-44' 
-          placeholder='Message' placeholderTextColor={"#CACCAC"} multiline={true} numberOfLines={1}
-          // onChangeText={handleTextChange}
-        />
+    <View style={{ flex: 1, backgroundColor: '#1F2937', paddingTop: 150 }}>
+      <View style={{ flex: 1, backgroundColor: '#718096', borderTopLeftRadius: 32, borderTopRightRadius: 32 }}>
+      </View>
+      <View style={{ padding: 20, position: 'absolute', bottom: 0, left: 0, right: 0, alignItems:"center" }}>
+      <TextInput className='z-10 border-2 px-4 w-96  rounded-3xl py-4 text-white border-neutral-100 h-auto max-h-44' 
+            placeholder='Message' placeholderTextColor={"#CACCAC"} multiline={true} numberOfLines={1} 
+            // onChangeText={handleTextChange}
+          />
       </View>
     </View>
   )
 }
+
+
+{/* <View style={{ flex: 1, backgroundColor: '#1F2937', paddingTop: 100, paddingHorizontal: 20 }}>
+      <View style={{ flex: 1, backgroundColor: '#718096', borderTopLeftRadius: 32, borderTopRightRadius: 32 }}>
+      </View>
+      <View style={{ padding: 20, paddingBottom: 0 }}>
+        <TextInput
+          style={{ backgroundColor: '#4A5568', borderRadius: 20, color: '#CACCAC', paddingHorizontal: 20, paddingVertical: 10 }}
+          placeholder='Message'
+          placeholderTextColor='#CACCAC'
+          multiline={true}
+          numberOfLines={1}
+          // onChangeText={handleTextChange}
+        />
+      </View>
+    </View> */}
+
+
+
+    // ---- my design 
+  
+  // <View className='flex-1 bg-stone-900 rounded-2xl relative pt-24'>
+
+  //       <View
+  //         className=" flex-1   bg-slate-200 "
+  //         style={{ borderTopLeftRadius: 32, borderTopRightRadius: 32 }}
+  //       >
+          
+  //       </View>
+
+  //       <View className='absolute bottom-0 left-0 right-0 px-4 pb-4 flex items-center justify-center'>
+  //         <TextInput className='z-10 border-2 px-4 w-96  rounded-3xl py-4 text-white border-neutral-100 h-auto max-h-44' 
+  //           placeholder='Message' placeholderTextColor={"#CACCAC"} multiline={true} numberOfLines={1} 
+  //           // onChangeText={handleTextChange}
+  //         />
+  //       </View>
+  //     </View> 
