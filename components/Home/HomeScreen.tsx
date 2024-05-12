f6485fb92c6949acdc31a46e4f49810871061aa9
Monish <monishsharma080@gmail.com>
2024-05-13 00:26:45 +0530

    made an abstract func and it is working file , finally

--- components/Home/HomeScreen.tsx
+++ components/Home/HomeScreen.tsx
@@ -6,9 +6,10 @@
   import JWTStore from '@/app/store';
   import { Redirect, useFocusEffect, useRouter } from 'expo-router';
   import PillShapeButtonForHomeScreen from './buttons/pillShapeButtonForHomeScreen';
-  import axios, { AxiosError } from 'axios';
-import axiosInstance from "../auth/utils/new_tokens_auth";
+//   import axios, { AxiosError } from 'axios';
+// import axiosInstance from "../auth/utils/new_tokens_auth";
 import { useQuery } from "@tanstack/react-query";
+import UpdateJWT, { temp_website } from "../auth/utils/functions_for_updating_tokens";
   
 
 
@@ -24,116 +25,25 @@ import { useQuery } from "@tanstack/react-query";
       },[JWT,makeARequest])
 
 
-    async function bb (){
-      let a = await axiosInstance.post("/temp_website",{
-        prompt :"Create website with many pages for a GYM that is on the way to create a revolution  ; give us a very  dope looking website that has too many colors as i ma trying to target the younger generation that like colors and photos and futuristic and modernly colorful, with animations"
-      },{headers:{Authorization: 'Bearer '+JSON.parse(getItem("JWT")).access}})
-      console.log("from the async function", await a);
+    // async function bb (){
+    //   let a = await axiosInstance.post("/temp_website",{
+    //     prompt :"Create website with many pages for a GYM that is on the way to create a revolution  ; give us a very  dope looking website that has too many colors as i ma trying to target the younger generation that like colors and photos and futuristic and modernly colorful, with animations"
+    //   },{headers:{Authorization: 'Bearer '+JSON.parse(getItem("JWT")).access}})
+    //   console.log("from the async function", await a);
       
-    }
-
-    
-    
-      async function UpdateJWT (setJWTTokens ){
-        const Jwt_string = getItem('JWT')
-       try {
-         console.log("in the update func --->>, my tokens -->>",JSON.parse(Jwt_string).refresh );
-         
-         const response = await axios.post(
-           `${process.env.EXPO_PUBLIC_BACKEND_URL}/api/token/refresh/`,
-           { refresh: JSON.parse(getItem('JWT')).refresh }
-         );
-         const { access, refresh } = response.data;
-         console.log("-------above the response ------");
-         
-         console.log("\n response data ----",response.headers);
-         
-         console.log("\n\n ================================8888888888888888-===========response and access(jsut to be sure ) -->>",refresh,"\n access -----", access);
-         deleteItemAsync("JWT").then(()=>{
-          // eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoicmVmcmVzaCIsImV4cCI6MTcxODI3NDQwNywiaWF0IjoxNzE1NTA5NjA3LCJqdGkiOiI3ZmI3ZGM0MzQ5Mjk0Zjk2ODk1ZWFhNGZkYjVkMGNiOCIsInVzZXJfaWQiOjR9.L_hqCl9ddWUuUnO2LcAwlFFLASbaEi2P8DyFlXcBols 
-          // access ----- eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzE1NTA5NjMyLCJpYXQiOjE3MTU1MDk1NjIsImp0aSI6Ijc1OTE1NmZlNWFjMTQ5MWM4ODRkM2NlYWVmMWViNjI5IiwidXNlcl9pZCI6NH0.h75SIL0z9vWXNmkmz2-07gmuJn2MgPkHA8PIJkLy1S4
-          setItem("JWT",JSON.stringify({access,refresh}))
-          setJWTTokens(JSON.stringify({access,refresh}))
-          
-        })
-        console.log("\n\n checking both the jwt tokens -->", Jwt_string,JSON.stringify({access,refresh}), "\n\n\n are both of these same -->>", JSON.stringify({access,refresh}) === Jwt_string );
-        refetch()
-      
-       } catch (error) {
-        console.log("\n -- in the error in updateJWT");
-        // ------------------------------------------ Alert -----------------------------------------------------
-        // alert the user and respond to the backend --or may be retry 
-        // ------------------------------------------ Alert -----------------------------------------------------
-        console.error(error)
-       }
-      }
-
-
-async function temp_website() {
-  let token = JSON.parse(getItem("JWT")).access
-
-    try {
-    const response = await fetch(`${process.env.EXPO_PUBLIC_BACKEND_URL}/temp_website`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + token,
-      },
-      body: JSON.stringify({
-        prompt: "Create website with many pages for a GYM that is on the way to create a revolution  ; give us a very  dope looking website that has too many colors as i ma trying to target the younger generation that like colors and photos and futuristic and modernly colorful, with animations"
-      })
-    });
-    if(response.status === 401){
-      UpdateJWT(setJWT)
-      refetch()
-    }
-
-    const body = await response.json();
-    // const text = await response.text();
-    console.log("Response data from temp_website:", body, "\n\nresponse -->",response, "\n\n --t--ex--t",);
-    setResponnseJSON(response)
-    setMakeARequest(false)
-    return {body,"headers":response};
-
-  } catch (error) {
-// 
-setMakeARequest(false)
-  if (String(error).includes("Network request failed")){
-  // ------- network error on client side 
-  Alert.alert("Network  error ", "Oops ! we are having trouble cause of your internet connection"  )
-}
-console.log("error form fetching --",String(error));
-
-
-    
-  }
-  // -------------------------------------- now make sure i also return the response from the 2 try after refresh token ------------------
-  // ----------------------------------------------------------
-  // ------------------------------------------  or maybe don't need to as Refetch works --- try an example to see  -->> yup I think it works 
-  // -------------------------------------------- lets make a abstract function
-  // --------------------------------------2.> setMakeARequest() .........3.> UpdateJWT()
-    // data_to_return_to_react_query 
-        
-    // if (data.status===401){
-    //   console.log("\n in the data two---");
-    //   UpdateJWT(setJWT)
-    //   // don't need to retuern form here as it already re runs the function
     // }
+
     
-    // setMakeARequest(false)
-
-  // -------------------------------------- now make sure i also return the response from the 2 try after refresh token ------------------
-
-
-}
+    
       
+
       
 
   // ------------------------------- react query ------------------------------
   
 const { isSuccess ,refetch, status, data}= useQuery({
   queryKey:['fetch-the-temp-website'],
-  queryFn: temp_website,
+  queryFn: ()=>temp_website("temp_website",setJWT,refetch,setMakeARequest,setResponnseJSON),
   enabled:makeARequest,
   // retry:2
   // pass the refetch and uspade state func in it 
