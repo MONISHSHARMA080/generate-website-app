030a58b6612da788fd983bf8096acd0bc266510b
Monish <monishsharma080@gmail.com>
2024-06-11 16:20:16 +0530

    removed sentry and kept the [osthog

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,7 +3,7 @@ import { Slot } from 'expo-router';
 import React  from 'react';
 import { PaperProvider } from 'react-native-paper';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import Constants from 'expo-constants';
+import { usePostHog, PostHogProvider } from 'posthog-react-native'
 
 
 export default function HomeLayout() {
@@ -17,7 +17,12 @@ export default function HomeLayout() {
        <QueryClientProvider client={queryClient}>     
         <PaperProvider>
           <SafeAreaView style={{flex:1, backgroundColor:"#010c1c"}}>
+          <PostHogProvider apiKey="phc_FaxP3jV1XlJNVzFW98VXtcj0v11PJUPXocegXioVpOq" options={{
+            host: "https://us.i.posthog.com",
+        }}>
+
               <Slot />
+          </PostHogProvider>
           </SafeAreaView>
         </PaperProvider> 
        </QueryClientProvider>
