0c6cfad851623a3083491b0be96ebe55250a6d55
monish <monishsharma080@gmail.com>
2024-07-07 14:53:35 +0530

    nothing much did some cleaning up, removed the unnecessary code , props,etc and moved the reanimated component (in the index.tsx-->> not tested but hopefully fine)

--- app/(main_app)/root_app/index.tsx
+++ app/(main_app)/root_app/index.tsx
@@ -7,34 +7,6 @@ import JWTStore from '@/app/store';
 
 export default function index() {
  
-  const { openDrawer, setOpenDrawer } = JWTStore();
-  const modalTranslateX = useSharedValue(0);
-  const [isActive, setIsActive] = useState(false);
-
-
-  React.useEffect(()=>{
-    console.log("\n\n ------ openDrawer, setOpenDrawer from index.tsx-",openDrawer, setOpenDrawer);
-    
-  },[isActive,setIsActive, openDrawer, setOpenDrawer])
-
-  React.useEffect(() => {
-    if (openDrawer) {
-      console.log(" we have the drawer in the index.tsx");
-      
-      modalTranslateX.value = withTiming(0, { duration: 300 });
-    } else {
-      modalTranslateX.value = withTiming(-Dimensions.get('window').width, { duration: 300 });
-    }
-  }, [
-    openDrawer,
-     modalTranslateX]);
-
-  const animatedStyle = useAnimatedStyle(() => {
-    return {
-      transform: [{ translateX: modalTranslateX.value }],
-    };
-  });
-
   return (
     <View style={{ flex: 1 }}>
       <HomeScreen    />
